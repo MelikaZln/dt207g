dc393599ecfa06e18f7a050873e9899d6c95084a
MelikaZln <159722410+MelikaZln@users.noreply.github.com>
2024-05-04 16:53:20 +0330

    comment install.js

--- install.js
+++ install.js
@@ -1,15 +1,16 @@
-require("dotenv").config();
+require("dotenv").config(); // Ladda miljövariabler från .env-filen
 const express = require("express");
-const sqlite3 = require("sqlite3").verbose();
+const sqlite3 = require("sqlite3").verbose(); // Importera SQLite-databasen
 
-
-// connect
+// anslutning till databasen med den angivna sökvägen från miljövariabler
 const db = new sqlite3.Database(process.env.DATABASE2);
 
-// create table users
+// skapa användartabellen 
 db.serialize(() => {
-    // drop table if exists 
+    // Radera tabellen om den redan existerar
     db.run("DROP TABLE IF EXISTS users");
+
+    // Skapa en ny användartabell med kolumner för användarnamn, lösenord, e-post och skapad datum/tid
     db.run(`CREATE TABLE users(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         username VARCHAR(255) NOT NULL UNIQUE,
@@ -17,5 +18,6 @@ db.serialize(() => {
         email VARCHAR(255) NOT NULL UNIQUE,
         created DATETIME DEFAULT CURRENT_TIMESTAMP
     )`);
-    console.log("TEBLE CREATED")
-})
+
+    console.log("TEBLE CREATED"); 
+});
